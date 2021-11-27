d1407500e6b9f37ff5fa0a7aeaedfdaac47351b3
Yoni Romm <yonatan.romm@sap.com>
2021-11-27 21:31:16 +0200

    added

---
+++ c/exec.sh
@@ -0,0 +1,2 @@
+gcc -ansi -Wall -Wextra -Werror -pedantic-errors -o hw1 hw1.c
+./hw1 3 100 input.txt output.txt
---
+++ c/hw1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <assert.h>
+
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+int read_file(char* input, int k){
+    FILE *ifp = NULL;
+    ifp = fopen(input,"r");
+    assert(ifp!=NULL);
+
+    fseek(ifp, 0, SEEK_END); // seek to end of file
+    long size = ftell(ifp); // get current file pointer
+    fseek(ifp, 0, SEEK_SET);
+
+    char buff[254];
+    fgets(buff, 255, (FILE*)ifp);
+    printf("print : %s\n", buff);
+    // fclose( ifp );
+}
+
+int main(int argc, char *argv[])  {
+    int k;
+    int max_iter;
+    
+    if (argc == 5){
+        sscanf(argv[1],"%d",&k);
+        sscanf(argv[2],"%d",&max_iter);
+        void* input_file = argv[3];
+        void* output_file = argv[4];
+    }
+    else if (argc == 4){
+        sscanf(argv[1],"%d",&k);
+        max_iter = 200;
+        void* input_file = argv[3];
+        void* output_file = argv[4];
+
+    }
+    else {
+        return 0;
+    }
+    read_file(argv[3],k);
+
+    return 1;
+}
+

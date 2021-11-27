284e0c4ae51f19d9827bfaa7ead7866586c9f30b
Yoni <yonik12@gmail.com>
2021-11-27 11:58:08 +0200

    removed the use of numpy library

--- main.py
+++ main.py
@@ -7,16 +7,25 @@ def algorithm(k, input_file, output_file, max_iter=200):
     centroids = init_centroids(data_points, k)
     N = len(data_points)
     while max_iter > 0:
-        clusters = [[] for i in range(k)]
+        clusters = [[] for _ in range(k)]
         for i in range(N):
             x_i = data_points[i]
             index = find_closest_cluster(x_i, centroids)
             clusters[index].append(x_i)
-        new_centroids = np.array([np.sum(clusters[i], axis=0) / len(clusters[i]) for i in range(k)])
+        new_centroids = [[] for i in range(k)]
+        for i in range(k):
+            cluster = clusters[i]
+            len_cluster = len(clusters[i])
+            current_centroid = [0 for _ in range(len(cluster[0]))]
+            for j in range(len_cluster):
+                data = cluster[j]
+                for m in range(len(data)):
+                    current_centroid[m] += data[m]
+            new_centroids[i] = [current_centroid[i] / len_cluster for i in range(len(current_centroid))]
         max_iter -= 1
-        diff_centroids = centroids - new_centroids
-        diff_centroids = np.array([np.sqrt(np.sum(np.square(diff_centroids[i]))) for i in range(len(diff_centroids))])
-        if np.max(diff_centroids) < epsilon:
+        diff_centroids = [[lst1[i] - lst2[i] for i in range(len(lst1))] for lst1, lst2 in zip(centroids, new_centroids)]
+        diff_centroids = [sum([num**2 for num in diff_centroids[i]])**0.5 for i in range(len(diff_centroids))]
+        if max(diff_centroids) < epsilon:
             break
         centroids = new_centroids
     writing_to_output_file(output_file, new_centroids)
@@ -24,23 +33,32 @@ def algorithm(k, input_file, output_file, max_iter=200):
 
 def writing_to_output_file(output_file, centroids):
     f = open(output_file, "a")
-    f.write(str(np.round(centroids, 4)))
+    f.write(centroids)
     f.close()
-
+    #’%.4f’
 
 def find_closest_cluster(data_point, mu_array):
-    difference_array = np.array([np.sum(np.square(data_point - mu_array[i])) for i in range(len(mu_array))])
-    return np.argmin(difference_array)
-
+    difference_array = [sum([(a - b)**2 for a, b in zip(data_point, mu_array[i])]) for i in range(len(mu_array))]
+    min_sum = difference_array[0]
+    index = 0
+    for i in range(len(difference_array)):
+        if difference_array[i] < min_sum:
+            min_sum = difference_array[i]
+            index = i
+    return index
 
 def reading_from_file(file):
     lines = tuple(open(file, 'r'))
-    vectors = np.array([np.array(line.split(",")) for line in lines]).astype(np.float)
+    vectors = [line.split(",") for line in lines]
+    for i in range(len(vectors)):
+        for j in range(len(vectors[i])):
+            vectors[i][j] = float(vectors[i][j])
+
     return vectors
 
 
 def init_centroids(vectors_array, k):
-    mu_array = np.array([vectors_array[i] for i in range(k)])
+    mu_array = [vectors_array[i] for i in range(k)]
     return mu_array
 
 

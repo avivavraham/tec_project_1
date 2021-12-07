40919cdee7d98a47016139da119fb4f69547abbb
Yoni Romm <yonik12@gmail.com>
2021-12-07 17:55:12 +0200

    refactor

--- c/kmeans_old.c
+++ c/kmeans_old.c
@@ -7,7 +7,7 @@
 
 void init_data_frame();
 
-void input_valid(int condition);
+void validate(int condition);
 
 int is_number(char s[]);
 
@@ -25,19 +25,19 @@ void algorithm();
 
 void init_centroids();
 
-void free_array_2d(double **arr, int r);
+void free_2d_array(double **arr, int r);
 
-void set_equal_array_2d(double **new, double **current, int r, int c);
+void set_equal_2d_array(double **new, double **current, int r, int c);
 
-void zero_array_2d(double **arr, int r, int c);
+void zero_2d_array(double **arr, int r, int c);
 
 void calculate_new_centroids();
 
-double **allocate_array_2d(int r, int c);
+double **allocate_2d_array(int r, int c);
 
-double calculate_distance_squared(double *a, double *b);
+double get_squared_distance(double *a, double *b);
 
-void error_occured(int condition);
+void error(int condition);
 
 void set_clusters();
 
@@ -47,24 +47,24 @@ char *input_file, *output_file;
 double **data_points, **centroids, **new_centroids, **clusters;
 
 int main(int argc, char **argv) {
-    input_valid(argc >= 4 && argc <= 5);
+    validate(argc == 4 || argc == 5);
 
-    input_valid(is_number(argv[1]));
+    validate(is_number(argv[1]));
 
 
-    input_valid(sscanf(argv[1], "%d", &k) != EOF);
-    input_valid(k > 1);
+    validate(sscanf(argv[1], "%d", &k) != EOF);
+    validate(k > 1);
 
     if (is_number(argv[2])) {
         sscanf(argv[2], "%d", &max_iter);
     } else {
-        input_valid(argc == 4);
+        validate(argc == 4);
         max_iter = 200;
     }
     input_file = argv[argc - 2];
     output_file = argv[argc - 1];
-    input_valid(!is_number(input_file));
-    input_valid(!is_number(output_file));
+    validate(!is_number(input_file));
+    validate(!is_number(output_file));
 
     init_data_frame();
 
@@ -78,16 +78,16 @@ void init_data_frame() {
     char line[1024];
 
     ifp = fopen(input_file, "r");
-    error_occured(ifp == NULL);
+    error(ifp == NULL);
 
     find_vectors_len(ifp);
     find_d_of_vector(ifp);
 
-    data_points = allocate_array_2d(num_rows, d);
-    error_occured(data_points == NULL);
+    data_points = allocate_2d_array(num_rows, d);
+    error(data_points == NULL);
 
     while (fgets(line, sizeof line, ifp) != NULL) {
-        line[strlen(line) - 1] = '\0';
+        line[strlen(line) - 1] = 0;
         for (j = 0; j < d; j++) {
             if (j == 0) {
                 data_points[i][j] = atof(strtok(line, ","));
@@ -95,14 +95,15 @@ void init_data_frame() {
                 data_points[i][j] = atof(strtok(NULL, ","));
             }
         }
+
         i++;
     }
     fclose(ifp);
 
 }
 
-void error_occured(int condition) {
-    if (condition == 1) {
+void error(int condition) {
+    if (condition) {
         printf("An Error Has Occurred\n");
         exit(1);
     }
@@ -114,8 +115,6 @@ void find_vectors_len(FILE *fp) {
         num_rows++;
     }
     fseek(fp, 0, SEEK_SET);
-
-    input_valid(k < num_rows);
 }
 
 void find_d_of_vector(FILE *fp) {
@@ -139,10 +138,10 @@ int find_closets_cluster(double *data_point) {
     int index = 0, i;
 
     difference = calloc(k, sizeof(int *));
-    error_occured(difference == NULL);
+    error(difference == NULL);
     for (i = 0; i < k; i++) {
         current_mu = centroids[i];
-        sum = calculate_distance_squared(current_mu, data_point);
+        sum = get_squared_distance(current_mu, data_point);
         difference[i] = sum;
     }
     min_sum = difference[0];
@@ -184,10 +183,10 @@ void calculate_new_centroids() {
     }
 }
 
-double calculate_distance_squared(double *a, double *b) {
+double get_squared_distance(double *a, double *b) {
     double distance = 0;
-    int i = d;
-    while (i--) {
+    int i;
+    for (i = 0; i < d; i++) {
         distance += pow(a[i] - b[i], 2);
     }
     return distance;
@@ -200,29 +199,29 @@ void algorithm() {
     double diff, sq_diff, max;
     num_elements_in_cluster = calloc(k, sizeof(int));
 
-    centroids = allocate_array_2d(k, d);
+    centroids = allocate_2d_array(k, d);
     init_centroids();
-    clusters = allocate_array_2d(k, d);
-    new_centroids = allocate_array_2d(k, d);
+    clusters = allocate_2d_array(k, d);
+    new_centroids = allocate_2d_array(k, d);
     sum_diff_centroids = calloc(k, sizeof(double));
     while (max_iter > 0) {
         for (j = 0; j < k; j++) {
             sum_diff_centroids[j] = 0;
         }
 
-        zero_array_2d(new_centroids, k, d);
-        zero_array_2d(clusters, k, d);
+        zero_2d_array(new_centroids, k, d);
+        zero_2d_array(clusters, k, d);
 
-        error_occured(num_elements_in_cluster == NULL);
+        error(num_elements_in_cluster == NULL);
 
         set_clusters();
         calculate_new_centroids();
 
         max_iter--;
-        error_occured(sum_diff_centroids == NULL);
+        error(sum_diff_centroids == NULL);
 
         for (i = 0; i < k; i++) {
-            diff = calculate_distance_squared(centroids[i], new_centroids[i]);
+            diff = get_squared_distance(centroids[i], new_centroids[i]);
             sq_diff = pow(diff, 0.5);
             sum_diff_centroids[i] = sq_diff;
         }
@@ -232,28 +231,30 @@ void algorithm() {
                 max = sum_diff_centroids[i];
             }
         }
-        set_equal_array_2d(centroids, new_centroids, k, d);
+        set_equal_2d_array(centroids, new_centroids, k, d);
         if (max <= epsilon) {
             max_iter = 0;
         }
     }
     write_to_output_file();
     free(sum_diff_centroids);
-    free_array_2d(centroids, k);
-    free_array_2d(new_centroids, k);
-    free_array_2d(data_points, num_rows);
-    free_array_2d(clusters, k);
+    free_2d_array(centroids, k);
+    free_2d_array(new_centroids, k);
+    free_2d_array(data_points, num_rows);
+    free_2d_array(clusters, k);
     free(num_elements_in_cluster);
 }
 
-double **allocate_array_2d(int r, int c) {
+double **allocate_2d_array(int r, int c) {
     double **arr;
     int i;
+
     arr = calloc(r, sizeof(double *));
-    error_occured(arr == NULL);
-    while (r--) {
-        arr[r] = calloc(c, sizeof(double));
-        error_occured(arr[r] == NULL);
+    error(arr == NULL);
+
+    for (i = 0; i < r; i++) {
+        arr[i] = calloc(c, sizeof(double));
+        error(arr[i] == NULL);
     }
     return arr;
 }
@@ -283,8 +284,8 @@ int isdigit_help(char digit) {
     return 1;
 }
 
-void input_valid(int condition) {
-    if (condition == 0) {
+void validate(int condition) {
+    if (!condition) {
         printf("Invalid Input!\n");
         exit(1);
     }
@@ -295,7 +296,7 @@ void write_to_output_file() {
     int i, j;
 
     fptr = fopen(output_file, "w");
-    error_occured(fptr == NULL);
+    error(fptr == NULL);
 
     for (i = 0; i < k; ++i) {
         for (j = 0; j < d - 1; ++j) {
@@ -308,14 +309,15 @@ void write_to_output_file() {
     fclose(fptr);
 }
 
-void free_array_2d(double **arr, int r) {
-    while (r--) {
-        free(arr[r]);
+void free_2d_array(double **arr, int r) {
+    int i;
+    for (i = 0; i < r; i++) {
+        free(arr[i]);
     }
     free(arr);
 }
 
-void set_equal_array_2d(double **new, double **current, int r, int c) {
+void set_equal_2d_array(double **new, double **current, int r, int c) {
     int i, j;
 
     for (i = 0; i < r; i++) {
@@ -325,7 +327,7 @@ void set_equal_array_2d(double **new, double **current, int r, int c) {
     }
 }
 
-void zero_array_2d(double **arr, int r, int c) {
+void zero_2d_array(double **arr, int r, int c) {
     int i, j;
 
     for (i = 0; i < r; i++) {
